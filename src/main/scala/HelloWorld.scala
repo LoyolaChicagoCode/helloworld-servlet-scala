7fc0eaf94d2f85c96398cc8106668258dc12b028
Konstantin Läufer <laufer@cs.luc.edu>
2012-01-14 00:33:48 -0600

    Modified to explore deployment environment.

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,6 +1,10 @@
 import javax.servlet.http._
 
 class HelloWorld extends HttpServlet {
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) =
-    resp.getWriter().print("Hello World!")
+  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) = { 
+    resp.getWriter.println("Hello World!")
+    resp.getWriter.println(new java.util.Date)
+    resp.getWriter.println(getServletContext.getInitParameter("oax"))
+    resp.getWriter.println(getServletContext.getClass)
+  }
 }
